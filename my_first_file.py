49ac6e5acbf46240def0105f29855524987f9930
furtadobb <furtadobb@gmail.com>
2019-01-31 10:59:05 -0200

    Aula 7

---
+++ my_first_file.py
@@ -0,0 +1,56 @@
+""" Files examples
+"""
+
+
+def first():
+    with open('experience.txt', 'w') as f:
+        f.write('This is my first saved output\n')
+
+
+def second():
+    with open('experience.txt', 'a') as f:
+        f.write('This is my second saved output\n')
+
+
+# Let's try with some numbers:
+def numbers():
+    l = list()
+    for i in range(10):
+        l.append(i ** i)
+
+    with open('experience.txt', 'a') as f:
+        for each in l:
+            f.write('{};'.format(str(each)))
+
+
+# What have we done?
+def reading():
+    with open('experience.txt', 'r') as f:
+        lida = f.read()
+    print(lida)
+    return lida
+
+
+def recover_list(li):
+    li = li.split('\n')
+
+    recovered = list()
+    for each in li[2].split(';'):
+        if len(each) > 0:
+            recovered.append(int(each))
+
+    print(recovered)
+    return recovered
+
+
+def sum_list(recovered):
+    print('A soma da lista recuperada é {:,.0f}'.format(sum(recovered)))
+
+
+if __name__ == '__main__':
+    first()
+    second()
+    numbers()
+    r = reading()
+    ll = recover_list(r)
+    sum_list(ll)
---
+++ my_first_pickle.py
@@ -0,0 +1,23 @@
+import pickle
+
+from my_first_file import reading, recover_list
+
+
+def save_pickle(ob):
+    with open('my_first_serialized_python_obj', 'wb') as f:
+        pickle.dump(ob, f)
+    print('Saved!!!')
+
+
+def load_pickle():
+    with open('my_first_serialized_python_obj', 'rb') as f:
+        ll = pickle.load(f)
+    print('A soma da lista lida é {}'.format(sum(ll)))
+    print('Lida!')
+
+
+if __name__ == '__main__':
+    r = reading()
+    l = recover_list(r)
+    save_pickle(l)
+    load_pickle()
