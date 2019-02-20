54261c74f8e9cb7d6ad320ebdcb3f4ad94cfe905
furtadobb <furtadobb@gmail.com>
2019-02-20 13:54:01 -0300

    ABM 1: Schelling

--- class_template.py
+++ class_template.py
@@ -1,15 +1,28 @@
+""" Class template
+    Ipea's Python for agent-based modeling course
+    """
+
+import random
+
+
 # class name typically Capital letter
 class Template:
+
     # Usually has an __init__ method called at the moment of instance creation
     def __init__(self, name, arg1, arg2):
+
         # Armazena os parâmetros de início dentro daquela instância
         # É comum ter uma ID de identificação única, ou nome
         self.id = name
         self.arg1 = arg1
         self.arg2 = arg2
+
         # Pode conter containers, data structures
         self.members = dict()
         self.ranking = list()
+
+        # Ou ainda, um valor randômico
+        self.luck = random.randrange(1, 60)
 
     def method1(self, quantia):
         # Modifica um valor armazenado
@@ -22,6 +35,11 @@ class Template:
         else:
             return False
 
+    def method3(self):
+        # Um método pode acessar um outro método.
+        # Nesse caso, adicionando um valor aleatório ao arg1!
+        self.method1(self.luck)
+
 
 if __name__ == '__main__':
     # Sempre TESTE
@@ -29,3 +47,5 @@ if __name__ == '__main__':
     b = Template(0, 50, 23)
     a.method1(25)
     print(b.method2(a))
+    print('Your lucky number is {}'.format(b.luck))
+    print('Your account increased by {}'.format(b.luck))
