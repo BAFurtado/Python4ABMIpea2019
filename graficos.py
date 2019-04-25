445646a82d99d5102c6fa18b962314c2ef64de36
furtadobb <furtadobb@gmail.com>
2019-04-25 16:42:26 -0300

    Graficos e mais graficos

---
+++ graficos.py
@@ -0,0 +1,19 @@
+""" Rápida demonstração gráficos
+"""
+
+
+# Sempre use a linha abaixo. É convenção Python
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Gerando dados aleatórios para trabalhar
+x = np.random.randn(10000)
+
+# 100 é o número de bins do histograma
+plt.hist(x, 100)
+
+# Note que aceita comandos tipo LaTeX -- entre símbolo dollar ($)
+plt.title('Normal distribution with $\mu=0, \sigma=1$')
+plt.savefig('meu_primeiro_histograma.png')
+plt.show()
+
---
+++ graficos2.py
@@ -0,0 +1,30 @@
+""" Segundo exemplo
+"""
+
+
+# Sempre use a linha abaixo. É convenção Python
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig1 = plt.figure()  # an empty figure with no axes
+
+x = np.linspace(0, 2, 100)
+
+plt.plot(x, x, label='linear')
+plt.plot(x, x**2, label='quadratic')
+plt.plot(x, x**3, label='cubic')
+
+plt.xlabel('x label')
+plt.ylabel('y label')
+
+plt.title("Simple Plot")
+plt.legend()
+plt.show()
+
+# Outro gráfico. Esse é o caso padrão, cria-se a figura e as modificações se dão no ax/axes
+
+x = np.arange(0, 10, 0.2)
+y = np.sin(x)
+fig2, ax = plt.subplots()
+ax.plot(x, y)
+plt.show()
