d9faaa23b9cc0f0ffc5c0e55721c143443ad6e2a
furtadobb <furtadobb@gmail.com>
2019-05-29 19:56:05 -0300

    Exemplo postos

--- Exemplo_postos/Final_Agentes.py
+++ Exemplo_postos/Final_Agentes.py
@@ -18,8 +18,9 @@ class Consumidores:
         self.capacity = 60
         self.level_gas = self.capacity * random.random()
 
-    def add_gas(self, volume):
-        self.level_gas += volume
+    def add_gas(self):
+        #volume = 60 - self.level_gas
+        self.level_gas = 60
 
     def drive(self):
         self.level_gas -= 6
@@ -32,33 +33,53 @@ class Consumidores:
             print('Go ahead')
             return False
 
+    #def bestprice(self):
+        #self
+
+
 
 class Postos:
     def __init__(self, p, area=None, vizinhos=None):
         self.identity = p
         self.tanque = random.randrange(200, 1000)
-        self.dist_cost = 0
+        self.dist_cost = 10
         self.loc = area
         self.vizinhos = vizinhos
+        self.preco = 10
 
-    def comprar(self, custo):
+    def check_estoque(self):
         if self.tanque <= 200:
-            self.tanque += 800
-            self.dist_cost = custo
+            return True
+        else:
+            return False
+
+    #def comprar(self, custo):
+    #    if self.tanque <= 200:
+    #        self.tanque += 1000 - self.tanque
+    #        self.dist_cost = custo
 
     def add_vizinhos(self, viz):
         self.vizinhos = viz
 
+    def set_distr_cost(self, preco):
+        self.dist_cost = preco
+
     def set_price(self):
-        media = sum([i.preco for i in self.vizinhos]) / len(self.vizinhos)
-
-
-
+        preco_vizinho = min([i.preco for i in self.vizinhos])
+        preco_posto = self.dist_cost * 1.05
+        if preco_posto > 1.05 * preco_vizinho:
+            preco_posto = preco_vizinho * (1 + (random.randrange(0, 5) / 100))
+        return preco_posto
 
 
 if __name__ == '__main__':
     d = Distribuidores()
     c = Consumidores(1)
-    p = Postos(2)
+    p1 = Postos(2)
+    p2 = Postos(3)
+    p3 = Postos(4)
+    viz = [p2, p3]
     c.check_gas()
-    print(p.tanque, c.level_gas)
+    p1.add_vizinhos(viz)
+    print('preço', p1.set_price())
+    print(p1.tanque, c.level_gas)
--- Exemplo_postos/Final_Interacao.py
+++ Exemplo_postos/Final_Interacao.py
@@ -2,7 +2,7 @@ from Final_Agentes import Distribuidores, Consumidores, Postos
 import random
 
 
-param = .1
+param = random.randrange(-10, 10) / 100
 
 
 def create(f, n):
@@ -17,19 +17,67 @@ def vizinhanca(postos):
         # Trocar para questão das áreas
         p.add_vizinhos(random.choices(postos, k=4))
 
+"""
+def abastece (list_cons, list_posto):
+    for d in range(120):
+        # carros testma nível de combustivel
+        # carros escolhem melhor preço e abastecem
+        # Carros consomem
+        for carro in consum:
+            if check_gas(carro) == False:
+                posto_vez = min(random.choices(list_posto, 3)
+                posto_vez.tanque -= 60 - carro.level_gas
+                carro.level_gas = 60                          
+            else:
+                carro.drive()
 
-def main(postos, consum, distribuidor):
+def estatisticas(dist, posto):
+    prdist = []
+    prcons = []
+    prposto = []
+    for d in range(120):
+        prdist.append(dist.custo)
+        for each in posto:
+            prcons.append
+"""
+
+
+def main_func(postos, consum, distribuidor):
+    # Recebe postos e consumidores
     # Adicionar vizinhos
     vizinhanca(postos)
     # Rodar os dias
-    for d in range(100):
+    for d in range(120):
+        #carros testma nível de combustivel
+        #carros escolhem melhor preço e abastecem
         # Carros consomem
+        # LISTA POSTOS, LISTA CONSUMIDORES = ABASTECE(LIS1, LIS2)
         for carro in consum:
-            carro.drive()
+            if carro.check_gas() is True:
+                postos_aconferir = random.sample(lista_postos, 3)
+                # Retornar o posto com preço mínimo.
+                posto_vez = 5
+                posto_vez.tanque -= (60 - carro.level_gas)
+                carro.add_gas()
+                carro.drive()
+            else:
+                carro.drive()
+
         # Distribuidora altera preços a cada 7 dias
         if d % 7 == 0:
             distribuidor.altera_custo(param)
+
         # Posto decide se compara gasolina
+        for loja in postos:
+            if loja.check_estoque is True:
+                loja.tanque = 1000
+                loja.set_price(distribuidor.custo)
+        return lista_postos, lista_consumidores, distribuidor
+
+        #Obtém preços
+
+
+
         # Posto modifica preço?
         # Consumidor verifica se compra gasolina
 
@@ -41,4 +89,4 @@ if __name__ == '__main__':
     lista_postos = create(Postos, 20)
     lista_consumidores = create(Consumidores, 1000)
     o_distribuidor = Distribuidores()
-    main(lista_postos, lista_consumidores, o_distribuidor)
+    main_func(lista_postos, lista_consumidores, o_distribuidor)
